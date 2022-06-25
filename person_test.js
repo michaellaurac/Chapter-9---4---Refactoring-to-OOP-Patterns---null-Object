42ae6480d1851a73428d1084d1b791347d9ad629
Michaël L <michaellaurac@gmail.com>
2022-06-25 21:56:06 +0200

    See previous commit

--- person_test.js
+++ person_test.js
@@ -17,9 +17,15 @@ describe("tests run on the 'person.js' file:", () => {
   const personOne = new Person("tony");
   const personTwo = new AnonymousPerson("tony");
   it("verifies personOne's name is tony", () => {
+    wish(personOne.name === "tony");
+  });
+  it("capitalizes personOne's name to Tony", () => {
     wish(capitalize(personOne.name) === "Tony");
   });
   it("verifies personTwo's name is null", () => {
+    wish(personTwo.name === null);
+  });
+  it("capitalizes  personTwo's name to null", () => {
     wish(capitalize(personTwo.name) === null);
   });
 });
